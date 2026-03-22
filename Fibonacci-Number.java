00ba098d1c2df09d79b71c7e18dda24587bab5c9
Samyak kumar singh <115411154+samyakkumarsingh@users.noreply.github.com>
2026-03-22 14:07:19 +0530

    [509] [Time Beats: 100.00%] [Memory Beats: 81.32%] - LeetPush

--- Fibonacci-Number.java
+++ Fibonacci-Number.java
@@ -1,8 +1,22 @@
 1class Solution {
-2    public int fib(int n) {
-3        if(n<=1)
-4        return n;
-5
-6        return fib(n-1)+fib(n-2);
-7    }
-8}
+2   
+3    public int fib(int n) {
+4         int [] dp=new int[n+1];
+5         for(int i = 0; i <= n; i++) {
+6            dp[i] = -1;
+7        }
+8
+9        return solve(n,dp);
+10    }
+11    public int solve(int n,int [] dp){
+12        if(n<=1)
+13        return n;
+14
+15        if(dp[n]!=-1)
+16        return dp[n];
+17
+18        dp[n]=solve(n-1,dp)+solve(n-2,dp);
+19
+20        return dp[n];
+21    }
+22}
