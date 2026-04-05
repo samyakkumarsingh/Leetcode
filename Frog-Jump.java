538b602e2339a4c832835ac4f870f65b677c39d1
Samyak kumar singh <115411154+samyakkumarsingh@users.noreply.github.com>
2026-04-06 00:34:43 +0530

    [403] [Time Beats: 9.26%] [Memory Beats: 5.05%] - LeetPush

--- Frog-Jump.java
+++ Frog-Jump.java
@@ -1,50 +1,46 @@
 1import java.util.*;
 2
 3class Solution {
-4    int n;
-5    HashMap<Integer, Integer> mp = new HashMap<>();
-6    int[][] t = new int[2001][2001];
+4    HashMap<Integer, Integer> mp = new HashMap<>();
+5    int[][] t = new int[2001][2001];
+6    int n;
 7
-8    public boolean solve(int[] stones, int currIndex, int prevJump) {
-9        if (currIndex == n - 1) {
-10            return true;
-11        }
-12
-13        if (t[currIndex][prevJump] != -1) {
-14            return t[currIndex][prevJump] == 1;
-15        }
-16
-17        boolean result = false;
-18
-19        for (int nextJump = prevJump - 1; nextJump <= prevJump + 1; nextJump++) {
-20            if (nextJump > 0) {
-21                int nextStone = stones[currIndex] + nextJump;
-22
-23                if (mp.containsKey(nextStone)) {
-24                    result = result || solve(stones, mp.get(nextStone), nextJump);
-25                }
-26            }
-27        }
-28
-29        t[currIndex][prevJump] = result ? 1 : 0;
-30        return result;
-31    }
-32
-33    public boolean canCross(int[] stones) {
-34        if (stones[1] != 1) {
-35            return false;
-36        }
-37
-38        n = stones.length;
+8    boolean solve(int[] stones, int i, int prevJump) {
+9        if (i == n - 1) return true;
+10
+11        if (t[i][prevJump] != -1)
+12            return t[i][prevJump] == 1;
+13
+14        for (int jump = prevJump - 1; jump <= prevJump + 1; jump++) {
+15            if (jump > 0) {
+16                int next = stones[i] + jump;
+17
+18                if (mp.containsKey(next)) {
+19                    if (solve(stones, mp.get(next), jump)) {
+20                        t[i][prevJump] = 1;
+21                        return true;
+22                    }
+23                }
+24            }
+25        }
+26
+27        t[i][prevJump] = 0;
+28        return false;
+29    }
+30
+31    public boolean canCross(int[] stones) {
+32        n = stones.length;
+33
+34        if (stones[1] != 1) return false;
+35
+36        for (int i = 0; i < n; i++) {
+37            mp.put(stones[i], i);
+38        }
 39
-40        for (int i = 0; i < n; i++) {
-41            mp.put(stones[i], i);
+40        for (int i = 0; i < 2001; i++) {
+41            Arrays.fill(t[i], -1);
 42        }
 43
-44        for (int[] row : t) {
-45            Arrays.fill(row, -1);
-46        }
-47
-48        return solve(stones, 0, 0);
-49    }
-50}
+44        return solve(stones, 0, 0);
+45    }
+46}
