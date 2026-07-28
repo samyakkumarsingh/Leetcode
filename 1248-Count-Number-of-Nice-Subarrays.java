17bc3e49bb3b6d49be48ef5d8bcdbab55f4848f6
Samyak kumar singh <115411154+samyakkumarsingh@users.noreply.github.com>
2026-07-28 10:37:03 +0530

    [1248] [Time Beats: 23.49%] [Memory Beats: 78.03%] - LeetPush

--- 1248-Count-Number-of-Nice-Subarrays.java
+++ 1248-Count-Number-of-Nice-Subarrays.java
@@ -1,36 +1,21 @@
 class Solution {
-    int[] arr;
     public int numberOfSubarrays(int[] nums, int k) {
-        arr=new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]%2==1){
-                arr[i]=1;
-            }
-            else arr[i]=0;
-        }
-        return subarraySum(arr,k);
-    }
-
-    public int subarraySum(int[] arr, int k) {
-        int cnt = 0;
-        int[] pre = new int[arr.length];
-        pre[0] = arr[0];
-
-        for (int i = 1; i < arr.length; i++) {
-            pre[i] = pre[i - 1] + arr[i];
-        }
+        int count = 0;
+        int prefix = 0;
 
         HashMap<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
 
-        for (int i = 0; i < pre.length; i++) {
-            int required = pre[i] - k;
+        for (int num : nums) {
+            prefix += num % 2;
 
-            cnt += map.getOrDefault(required, 0);
+            int required = prefix - k;
 
-            map.put(pre[i], map.getOrDefault(pre[i], 0) + 1);
+            count += map.getOrDefault(required, 0);
+
+            map.put(prefix, map.getOrDefault(prefix, 0) + 1);
         }
 
-        return cnt;
+        return count;
     }
 }
