411eab4ca42614d0b0b44c1a5dc3a8d083ed2b81
Samyak kumar singh <115411154+samyakkumarsingh@users.noreply.github.com>
2026-07-31 12:43:46 +0530

    [70] [Time Beats: 100.00%] [Memory Beats: 73.40%] - LeetPush

---
+++ 70-Climbing-Stairs.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class Solution {
+
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+
+        return solve(n, dp);
+    }
+
+    public int solve(int n, int[] dp) {
+        if (n == 0) return 1;
+        if (n == 1) return 1;
+
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+
+        return dp[n];
+    }
+}
