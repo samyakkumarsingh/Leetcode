2700373c93befce1fbe455e3b2ef78c77306f6b9
Samyak kumar singh <115411154+samyakkumarsingh@users.noreply.github.com>
2026-08-03 12:14:25 +0530

    [78] [Time Beats: 86.83%] [Memory Beats: 88.91%] - LeetPush

--- 78-Subsets.java
+++ 78-Subsets.java
@@ -9,24 +9,13 @@ class Solution {
 
     public void solve(int[] nums, int start, List<Integer> temp) {
 
-        // Base case: all elements have been considered
         if (start == nums.length) {
             result.add(new ArrayList<>(temp));
             return;
         }
-
-        // Include current element
         temp.add(nums[start]);
         solve(nums, start + 1, temp);
-
-        // ❌ Your mistake:
-        // temp.remove(nums[nums.length]);
-        //
-        // nums[nums.length] is OUT OF BOUNDS because the last valid index is nums.length - 1.
-        // Also, you should remove the LAST ADDED ELEMENT, not an element from nums.
         temp.remove(temp.size() - 1);
-
-        // Exclude current element
         solve(nums, start + 1, temp);
     }
 }
