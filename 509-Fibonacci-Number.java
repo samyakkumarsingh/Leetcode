1ab4cc00eab02648ef3c40b49b7376c16b178fc1
Samyak kumar singh <115411154+samyakkumarsingh@users.noreply.github.com>
2026-08-18 13:03:26 +0530

    [509] [Time Beats: 100.00%] [Memory Beats: 99.80%] - LeetPush

--- 509-Fibonacci-Number.java
+++ 509-Fibonacci-Number.java
@@ -1,8 +1,22 @@
 class Solution {
     public int fib(int n) {
-        if(n<=1)
-        return n;
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
 
-        return fib(n-1)+fib(n-2);
+        return solve(n, dp);
+    }
+
+    private int solve(int n, int[] dp) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+
+        return dp[n];
     }
 }
