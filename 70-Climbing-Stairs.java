b9d97d1ae4964d524126af096e8d26d6dbbe68dd
Samyak kumar singh <115411154+samyakkumarsingh@users.noreply.github.com>
2026-08-18 12:47:50 +0530

    [70] [Time Beats: 100.00%] [Memory Beats: 36.62%] - LeetPush

--- 70-Climbing-Stairs.java
+++ 70-Climbing-Stairs.java
@@ -1,38 +1,19 @@
-// import java.util.Arrays;
-
-// class Solution {
-
-//     public int climbStairs(int n) {
-//         int[] dp = new int[n + 1];
-//         Arrays.fill(dp, -1);
-
-//         return solve(n, dp);
-//     }
-
-//     public int solve(int n, int[] dp) {
-//         if (n == 0) return 1;
-//         if (n == 1) return 1;
-
-//         if (dp[n] != -1) {
-//             return dp[n];
-//         }
-
-//         dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
-//         return dp[n];
-//     }
-// }
-
-
 class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[n + 1];
-        dp[0] = 1;
-        if (n >= 1) {
-            dp[1] = 1;
+        return solve(n, dp);
+    }
+
+    public int solve(int n, int[] dp) {
+        if (n == 0 || n == 1) {
+            return 1;
         }
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+
+        if (dp[n] != 0) {
+            return dp[n];
         }
+        dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+
         return dp[n];
     }
 }
